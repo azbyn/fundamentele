53572ee26388a632bc6b49e96dd1bf5cb0200566
azbyn <andreypavel1@yahoo.com>
2018-10-07 13:01:56 +0300

    Added gitignore

--- base.cpp
+++ base.cpp
@@ -1,29 +1,6 @@
 #include <iostream>
 #include <algorithm>
 
-std::string toBase2(unsigned n) {
-    if (n == 0) return "0";
-    std::string res = "";
-    while (n) {
-        res += ('0' + (n % 2));
-        n /= 2;
-    }
-    std::reverse(res.begin(), res.end());
-    return res;
-}
-
-
-std::string toBase16(unsigned n) {
-    if (n == 0) return "0";
-    std::string res = "";
-    while (n) {
-        int mod = n % 16;
-        res += (mod < 10) ? ('0' + mod) : ('A' - 10 + mod);
-        n /= 16;
-    }
-    std::reverse(res.begin(), res.end());
-    return res;
-}
 
 int main() {
     int n;
@@ -32,15 +9,29 @@ int main() {
         std::cerr << "That was not a number!\n";
         return -1;
     }
-    /* 
-    //WIP
-    std::cout << n << "is ";
+    std::cout << n << " is '";
+    if (n == 0) {
+        std::cout << "0' in base 2 and '0' in base 16 \n";
+        return 0;
+    }
+    // base 2
+    unsigned pow2 = 1 << (sizeof(unsigned) * 8 - 1);
+    while (!(n & pow2)) pow2 >>= 1;
+    while (pow2) {
+        std::cout << (char)('0' + (!!(n & pow2)));
+        pow2 >>= 1;
+    }
+    std::cout << "' in base 2 and '";
+    // base 16
+    std::string res = "";
+    while (n) {
+        int mod = n % 16;
+        res += (mod < 10) ? ('0' + mod) : ('A' - 10 + mod);
+        n /= 16;
+    }
+    std::reverse(res.begin(), res.end());
 
-    int pow2 = 1 << (sizeof(int) * 8 - 1);
-    while (! (n & )  )
-    */
+    std::cout << res << "' in base 16\n";
 
-    std::cout << n << " is '" << toBase2(n) << "' in base 2 and '"
-              << toBase16(n) << "' in base 16\n";
     return 0;
 }
