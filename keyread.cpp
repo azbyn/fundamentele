3daa35b3b3c21a0e1bbe828eabc56a7f710c1934
azbyn <andreypavel1@yahoo.com>
2018-12-08 22:06:32 +0200

    updated keyread

--- keyread.cpp
+++ keyread.cpp
@@ -8,11 +8,19 @@
 #include <linux/kd.h>
 #include <linux/keyboard.h>
 #include <sys/ioctl.h>
+#include <linux/input-event-codes.h>
 
 //#include <locale.h>
 #include <errno.h>
 
-/*extern */int getfd(const char *fnam);
+#include <stdexcept>
+
+
+#define logfln(x, ...) fprintf(stderr, "> " x "\n", __VA_ARGS__)
+#define logf(x, ...) fprintf(stderr, x, __VA_ARGS__)
+
+
+/*extern */int getfd();
 
 int fd;
 int oldkbmode;
@@ -21,14 +29,14 @@ struct termios old;
 * version 0.81 of showkey would restore kbmode unconditially to XLATE,
 * thus making the console unusable when it was called under X.
 */
-static void
-get_mode(void) {
+static int get_mode() {
+    int mode;
     const char *m;
-    if (ioctl(fd, KDGKBMODE, &oldkbmode)) {
+    if (ioctl(fd, KDGKBMODE, &mode)) {
         perror("KDGKBMODE");
         exit(1);
     }
-    switch(oldkbmode) {
+    switch(mode) {
     case K_RAW:
         m = "RAW"; break;
     case K_XLATE:
@@ -41,13 +49,14 @@ get_mode(void) {
         m = "?UNKNOWN?"; break;
     }
     printf("kb mode was %s\n", m);
-    if (oldkbmode != K_XLATE) {
+    if (mode != K_XLATE) {
         printf("[ if you are trying this under X, it might not work\n"
             "since the X server is also reading /dev/console ]\n");
     }
     printf("\n");
+    return mode;
 }
-static void clean_up(void) {
+static void clean_up() {
     if (ioctl(fd, KDSKBMODE, oldkbmode)) {
         perror("KDSKBMODE");
         exit(1);
@@ -61,26 +70,8 @@ static void die(int x) {
     clean_up();
     exit(1);
 }
-static void watch_dog(int /*x*/) {
-    clean_up();
-    exit(0);
-}
-
-int main(/*int argc, char *argv[]*/) {
-    int show_keycodes = 1;
-    struct termios new_;
-    unsigned char buf[16];
-    int i, n;
-    //Likely useless:
-    //setlocale(LC_ALL, "");
-
-    fd = getfd(NULL);
-    /* the program terminates when there is no input for 10 secs */
-    signal(SIGALRM, watch_dog);
-    /*
-        if we receive a signal, we want to exit nicely, in
-        order not to leave the keyboard in an unusable mode
-    */
+void init_signal_handler() {
+    signal(SIGALRM, die);
     signal(SIGHUP, die);
     signal(SIGINT, die);
     signal(SIGQUIT, die);
@@ -104,36 +95,79 @@ int main(/*int argc, char *argv[]*/) {
     signal(SIGTSTP, die);
     signal(SIGTTIN, die);
     signal(SIGTTOU, die);
-    get_mode();
+}
+
+int main(/*int argc, char *argv[]*/) {
+    struct termios new_;
+    unsigned char buf[16];
+    bool show_keycodes = true;
+    //Likely useless:
+    //setlocale(LC_ALL, "");
+
+    fd = getfd();
+    init_signal_handler();
+
+    oldkbmode = get_mode();
     if (tcgetattr(fd, &old) == -1)
         perror("tcgetattr");
     if (tcgetattr(fd, &new_) == -1)
         perror("tcgetattr");
-    new_.c_lflag &= ~ (ICANON | ECHO | ISIG);
+    new_.c_lflag &= ~(ICANON | ECHO | ISIG);
     new_.c_iflag = 0;
     new_.c_cc[VMIN] = sizeof(buf);
-    new_.c_cc[VTIME] = 1;	/* 0.1 sec intercharacter timeout */
+    new_.c_cc[VTIME] = 1; /* 0.1 sec intercharacter timeout */
     if (tcsetattr(fd, TCSAFLUSH, &new_) == -1)
         perror("tcsetattr");
     if (ioctl(fd, KDSKBMODE, show_keycodes ? K_MEDIUMRAW : K_RAW)) {
         perror("KDSKBMODE");
         exit(1);
     }
-    printf("press any key (program terminates 10s after last keypress)...\n");
+
+    constexpr int modKeysCount = 3;
+    constexpr uint8_t modKeys[modKeysCount][2] = {
+        {KEY_LEFTSHIFT, KEY_RIGHTSHIFT},
+        {KEY_LEFTCTRL, KEY_RIGHTCTRL},
+        {KEY_LEFTMETA, KEY_RIGHTMETA}
+    };
+
+    bool modPresses[modKeysCount][2] = {};
+    auto hasModPress = [&modPresses] (int i) { return modPresses[i][0] || modPresses[i][1]; };
+    const int modkey = KEY_CAPSLOCK; // 125;
+    bool pressing_mod = false;
+
     for (;;) {
-        alarm(10);
-        n = read(fd, buf, sizeof(buf));
-        for (i = 0; i < n; i++) {
-            if (!show_keycodes)
-                printf("0x%02x ", buf[i]);
-            else
-                printf("keycode %3d %s\n",
-                    buf[i] & 0x7f,
-                    buf[i] & 0x80 ? "release"
-                                    : "press");
+        int n = read(fd, buf, sizeof(buf));
+        for (int i = 0; i < n; i++) {
+            char keycode = buf[i] & 0x7f;
+            bool released = buf[i] & 0x80;
+            for (int j = 0; j < modKeysCount; ++j) {
+                for (int k = 0; k < 2; ++k){
+                    if (keycode == modKeys[j][k]) {
+                        modPresses[j][k] = !released;
+                        goto endModkeySearch;
+                    }
+                }
+            }
+
+            printf("key ");
+            if (hasModPress(0)) printf("SHIFT-");
+            if (hasModPress(1)) printf("CTRL-");
+            if (hasModPress(2)) printf("META-");
+
+            printf("%3d\n", keycode);
+
+
+        endModkeySearch:
+
+            if (keycode == modkey) {
+                if (pressing_mod != !released)
+                    logf("\n<%smod>\n", released ? "\\" : "");
+                pressing_mod = !released;
+            }
+            else if (keycode == 0) break;
+            else {//if (pressing_mod && !released) {
+            }
         }
-        if (!show_keycodes)
-            printf("\n");
     }
     clean_up();
 
@@ -149,15 +183,13 @@ int main(/*int argc, char *argv[]*/) {
 * We try several things because opening /dev/console will fail
 * if someone else used X (which does a chown on /dev/console).
 */
-static int
-is_a_console(int fd) {
+static int is_a_console(int fd) {
     char arg;
     arg = 0;
-    return (ioctl(fd, KDGKBTYPE, &arg) == 0
-        && ((arg == KB_101) || (arg == KB_84)));
+    return (ioctl(fd, KDGKBTYPE, &arg) == 0 &&
+            ((arg == KB_101) || (arg == KB_84)));
 }
-static int
-open_a_console(const char *fnam) {
+static int open_a_console(const char *fnam) {
     int fd;
     /*
     * For ioctl purposes we only need some fd and permissions
@@ -176,32 +208,26 @@ open_a_console(const char *fnam) {
     }
     return fd;
 }
-int getfd(const char *fnam) {
-    int fd;
-    if (fnam) {
-        fd = open_a_console(fnam);
-        if (fd >= 0)
-            return fd;
-        fprintf(stderr,
-            "Couldnt open %s\n", fnam);
-        exit(1);
+#define GETFD_IMPL(_path)               \
+    fd = open_a_console(_path);         \
+    if (fd >= 0) {                      \
+        logfln("Opened '%s'", _path);    \
+        return fd;                      \
     }
-    fd = open_a_console("/dev/tty");
-    if (fd >= 0)
-        return fd;
-    fd = open_a_console("/dev/tty0");
-    if (fd >= 0)
-        return fd;
-    fd = open_a_console("/dev/vc/0");
-    if (fd >= 0)
-        return fd;
-    fd = open_a_console("/dev/console");
-    if (fd >= 0)
-        return fd;
-    for (fd = 0; fd < 3; fd++)
-        if (is_a_console(fd))
+
+int getfd() {
+    int fd;
+    GETFD_IMPL("/dev/tty");
+    GETFD_IMPL("/dev/tty0");
+    GETFD_IMPL("/dev/vc/0");
+    GETFD_IMPL("/dev/console");
+
+    for (fd = 0; fd < 3; fd++) {
+        if (is_a_console(fd)) {
+            logfln("Opened '%d'", fd);
             return fd;
-    fprintf(stderr,
-        "Couldnt get a file descriptor referring to the console\n");
-    exit(1);		/* total failure */
+        }
+    }
+    throw std::runtime_error("Couldnt get a file descriptor referring to the console\n");
 }
+#undef GETFD_IMPL
